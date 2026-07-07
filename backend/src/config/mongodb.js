524d1b848ded8e3428b09ef02c8cc9f6f4e483a1
Prem <prem@example.com>
2026-07-07 21:37:38 +0530

    db

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/madurai_for_nation';
+    const uri = process.env.MONGODB_URI ;
 
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
